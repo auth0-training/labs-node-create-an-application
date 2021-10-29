96e269c7f5de749473f7498262078f870ba653f8
Bobby Johnson <bobby.johnson@gmail.com>
2021-10-29 15:17:03 -0700

    updating session secret to warning message

--- src/web-app/env-config.js
+++ src/web-app/env-config.js
@@ -3,7 +3,7 @@ const {
   API_URL,
   CLIENT_ID,
   CLIENT_SECRET,
-  SESSION_SECRET = "e6de1374433dde92a5bae567b56b01de32ad0d4f0115b7675f5b985720210f4c",
+  SESSION_SECRET = 'a long, randomly-generated string stored in env',
   PORT = 7000,
 } = process.env;
 
@@ -20,13 +20,13 @@ function checkUrl() {
 }
 
 function removeTrailingSlashFromUrl(url) {
-  if (!url || !url.endsWith("/")) return url;
+  if (!url || !url.endsWith('/')) return url;
 
   return url.substring(0, url.length - 1);
 }
 
-console.log("\n----------------------------------");
-console.log("Envronment Settings:");
+console.log('\n----------------------------------');
+console.log('Envronment Settings:');
 console.log(`ISSUER_BASE_URL: ${ISSUER_BASE_URL}`);
 console.log(`API_URL: ${API_URL}`);
 console.log(`CLIENT_ID: ${CLIENT_ID}`);
@@ -34,7 +34,7 @@ if (CLIENT_SECRET) console.log(`CLIENT_SECRET: Has Value`);
 else console.log(`CLIENT_SECRET: Not Set`);
 console.log(`APP_URL: ${appUrl}`);
 console.log(`SESSION_SECRET: ${SESSION_SECRET}`);
-console.log("----------------------------------\n");
+console.log('----------------------------------\n');
 
 module.exports = {
   checkUrl,
